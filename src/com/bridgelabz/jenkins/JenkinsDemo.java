91fe6b08728c77c15d31c5fddd010f848a4778a5
BL-AnniSingh <anni.singh@bridgelabz>
2026-03-18 22:38:25 +0530

    second commit

--- src/com/bridgelabz/jenkins/JenkinsDemo.java
+++ src/com/bridgelabz/jenkins/JenkinsDemo.java
@@ -4,5 +4,6 @@ public class JenkinsDemo {
     public  static void main(String[] args) {
 
         System.out.println("Hello world..i am learning jenkins");
+        System.out.println("Hello world .............i am testing jenkins");
     }
 }
